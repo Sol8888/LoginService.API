fad181aad4b124761dc2d1309a8b93666eaf95d2
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-06-10 17:46:58 -0500

    Cambios en el orden del request

--- LoginService.API/API/Models/LoginRequest.cs
+++ LoginService.API/API/Models/LoginRequest.cs
@@ -5,7 +5,8 @@ namespace LoginService.API.API.Models
 {
     public class LoginRequest
     {
-        public string Password { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        
     }
 }
